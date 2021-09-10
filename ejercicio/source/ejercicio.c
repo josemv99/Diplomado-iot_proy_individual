c86727712bc0fbf9e72def223b11ccfc25279c1d
JoseM <josemiguelmv99@gmail.com>
2021-09-10 11:26:37 -0500

    ejemplo
    actividad entregable 2

--- ejercicio/source/ejercicio.c
+++ ejercicio/source/ejercicio.c
@@ -22,6 +22,7 @@
 #include "sensor_de_luz.h"
 #include "irq_lptmr0.h"
 #include "botones.h"
+#include "sensor_de_temperatura.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -64,21 +65,46 @@ int main(void) {
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
+    volatile char boton1_aux, boton2_aux;
     bool boton1,boton2;
     uint32_t sensor_de_luz;
+    uint32_t sensor_de_temperatura;
+    float volt_temperatura, volt_luz;
+    unsigned char temperatura,iluminancia;
     /* Enter an infinite loop, just incrementing a counter. */
     LPTMR_StartTimer(LPTMR0);
     while(1) {
     	if(lptmr0_irq_counter != 0){
-    		toggle_led_red();
     		lptmr0_irq_counter=0;
-            i++ ;
-            sensor_de_luz = sensorDeLuzObtenerDatoADC();
-            //PRINTF("adc: %u\r\n ", sensor_de_luz);
+    		i++ ;
+    		sensor_de_temperatura = sensorDeTemperaturaObtenerDatoADC();
+    		volt_temperatura = (3 * sensor_de_temperatura)/4095;
+    		temperatura = (volt_temperatura * 90)/3.3;
+    		sensor_de_luz = sensorDeLuzObtenerDatoADC();
+    		volt_luz = (3.3 * sensor_de_luz);
+    		iluminancia = (2*(3.3 - volt_luz))*100;
             boton1 = boton1LeerEstado();
             boton2 = boton2LeerEstado();
-            PRINTF("boton1: %u\r\n",boton1);
-            PRINTF("boton2: %u\r\n",boton2);
+          if (boton1 && !boton1_aux){
+            	boton1_aux = 1;
+            	PRINTF("la temperatura es: %d", temperatura);
+            	PRINTF(" °C\r\n");
+            }
+           if (!boton1){
+        	   boton1_aux = 0;
+           }
+             if (boton2 && !boton2_aux){
+             	boton2_aux = 1;
+            	PRINTF("la intensidad de luz es: %d", iluminancia);
+            	PRINTF(" Lux\r\n");
+            }
+             if (!boton2){
+                boton2_aux = 0;
+             }
+            if (LR_counter == 1000){
+            	LR_counter = 0;
+            	toggle_led_red();
+            }
     	}
         /* 'Dummy' NOP to allow source level single stepping of
             tight while() loop */
