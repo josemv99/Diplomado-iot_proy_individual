60c7d5c3e0eeae1119a023a645b00bdd14057558
JoseM <josemiguelmv99@gmail.com>
2021-09-11 12:36:14 -0500

    configuraciones
    adecuaciones del codigo

--- ejercicio/board/peripherals.h
+++ ejercicio/board/peripherals.h
@@ -12,6 +12,8 @@
 #include "fsl_common.h"
 #include "fsl_adc16.h"
 #include "fsl_lptmr.h"
+#include "fsl_lpuart.h"
+#include "fsl_clock.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -43,13 +45,21 @@ extern "C" {
 /* Definition of the prescaled clock source frequency */
 #define LPTMR0_INPUT_FREQ 1000UL
 /* Definition of the timer period in us */
-#define LPTMR0_USEC_COUNT 10000UL
+#define LPTMR0_USEC_COUNT 1000000UL
 /* Definition of the timer period in number of ticks */
-#define LPTMR0_TICKS 10UL
+#define LPTMR0_TICKS 1000UL
 /* LPTMR0 interrupt vector ID (number). */
 #define LPTMR0_IRQN LPTMR0_IRQn
 /* LPTMR0 interrupt handler identifier. */
 #define LPTMR0_IRQHANDLER LPTMR0_IRQHandler
+/* Definition of peripheral ID */
+#define LPUART0_PERIPHERAL LPUART0
+/* Definition of the clock source frequency */
+#define LPUART0_CLOCK_SOURCE 48000000UL
+/* LPUART0 interrupt vector ID (number). */
+#define LPUART0_SERIAL_RX_TX_IRQN LPUART0_IRQn
+/* LPUART0 interrupt handler identifier. */
+#define LPUART0_SERIAL_RX_TX_IRQHANDLER LPUART0_IRQHandler
 
 /***********************************************************************************************************************
  * Global variables
@@ -58,6 +68,7 @@ extern adc16_channel_config_t ADC0_channelsConfig[2];
 extern const adc16_config_t ADC0_config;
 extern const adc16_channel_mux_mode_t ADC0_muxMode;
 extern const lptmr_config_t LPTMR0_config;
+extern const lpuart_config_t LPUART0_config;
 
 /***********************************************************************************************************************
  * Initialization functions
